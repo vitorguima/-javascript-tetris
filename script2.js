870be45e4ca3485b62c86d22151861d295d47991
Vitor <vitorguima@gmail.com>
2021-04-28 11:22:05 -0300

    drawing random pieces in the start position done

--- script2.js
+++ script2.js
@@ -1,51 +1,49 @@
 const getPaintBoard = document.getElementById('pixel-board');
 const getHeightDiv = document.getElementsByClassName('tr');
-const tetrisBoard = document.getElementsByClassName('pixel');
+const tetrisPixel = document.getElementsByClassName('pixel');
 const startButton = document.querySelector('#start-game');
 const width = 10;
+
 
 function start() {
 	generateHeightDivs(20);
 	generateWidthDivs(10);
-	addWhiteToBoard();
 }
 
 window.onload = start();
 
-const tetrisBoardArray = Array.from(tetrisBoard);
-
-// peaces of the game
-const lPeace = [
-	[1, width + 1, width * 2 + 1, width * 2],
-	[width, width * 2, width * 2 + 1, width * 2 + 2],
-	[width, width + 1, width + 2, width * 2 + 2],
+// pieces of the game
+const lPiece = [
+	[1, width + 1, width * 2, width * 2 + 1],
+	[0, width, width + 1, width + 2],
+	[0, 1, 2, width + 2],
 	[1, 2, width + 1, width * 2 + 1]
 ];
-const zPeace = [
-	[width + 1, width + 2, width * 2, width * 2 + 1],
+const zPiece = [
+	[1, 2, width, width + 1], 
 	[0, width, width + 1, width * 2 + 1],
-	[width + 1, width + 2, width * 2, width * 2 + 1],
+	[1, 2, width, width + 1],
 	[0, width, width + 1, width * 2 + 1]
 ];
 const tPeace = [
 	[1, width, width + 1, width + 2],
 	[1, width + 1, width + 2, width * 2 + 1],
-	[width, width + 1, width + 2, width * 2 + 1],
+	[0, 1, 2, width + 1],
 	[1, width, width + 1, width * 2 + 1]
 ];
-const oPeace = [
-	[width, width + 1, width * 2, width * 2 + 1],
-	[width, width + 1, width * 2, width * 2 + 1],
-	[width, width + 1, width * 2, width * 2 + 1],
-	[width, width + 1, width * 2, width * 2 + 1]
+const oPiece = [
+	[0, 1, width, width + 1],
+	[0, 1, width, width + 1],
+	[0, 1, width, width + 1],
+	[0, 1, width, width + 1]
 ]
-const iPeace = [
+const iPiece = [
 	[0, width, width * 2, width * 3],
-	[width * 3, width * 3 + 1, width * 3 + 2, width * 3 + 3],
+	[0, 1, 2, 3],
 	[0, width, width * 2, width * 3],
-	[width * 3, width * 3 + 1, width * 3 + 2, width * 3 + 3]
+	[0, 1, 2, 3]
 ];
-const peacesArray = [lPeace, zPeace, tPeace, oPeace, iPeace];
+const piecesArray = [lPiece, zPiece, tPeace, oPiece, iPiece];
 
 // generate pixel table
 function generateHeightDivs(number) {
@@ -68,22 +66,20 @@ function generateWidthDivs(number) {
 	}
 }
 
-// add white background to table
-function addWhiteToBoard() {
-	for (let index = 0; index < tetrisBoard.length; index += 1) {
-		tetrisBoard[index].style.backgroundColor = 'rgb(255, 255, 255)';
+// where the tetris piece start
+let currentPosition = 4;
+
+// randomize piece and its position
+let currentPiece = piecesArray[Math.floor(Math.random() * 5)][Math.floor(Math.random() * 4)];
+
+const tetrisPixelArray = Array.from(tetrisPixel);
+
+const draw = () => {
+	for (index of currentPiece) {
+		tetrisPixelArray[currentPosition + index].classList.add('paint-piece')
 	}
 }
 
-// paint peaces shape
-function paintPeaces() {
-	for (let index = 0; index < tetrisBoardArray; index += 1) {
-		tetrisBoardArray[peacesArray[0][0].className = 'paint-peace'];
-	}
-	console.log(tetrisBoardArray);
-	console.log(peacesArray[0][0]);
-} 
-
-paintPeaces();
+draw();
 
 
